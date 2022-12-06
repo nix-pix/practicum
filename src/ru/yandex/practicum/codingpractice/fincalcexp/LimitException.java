8a8975a49a92387cedf59a6e9b320d966dda0913
nix-pix <nix-pix@yandex.ru>
2022-12-06 20:56:26 +0300

    Добавлен FinancialCalculatorException

---
+++ src/ru/yandex/practicum/codingpractice/fincalcexp/LimitException.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.codingpractice.fincalcexp;
+
+public class LimitException extends RuntimeException {
+    final int attempts;
+
+    public LimitException(final String message, final int attempts) {
+        super(message);
+        this.attempts = attempts;
+    }
+}
